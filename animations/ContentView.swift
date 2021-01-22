b2cef50177fd39c78b04ff92a2bf2d953b2e442b
cejimenez0123 <christianjimenez0123@gmail.com>
2021-01-22 12:44:01 -0500

    m

--- animations/ContentView.swift
+++ animations/ContentView.swift
@@ -8,23 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
+    let letters = Array("Hello SwiftUI")
+       @State private var enabled = false
+       @State private var dragAmount = CGSize.zero
+  
     var body: some View {
-        Button("Tap Me",action: {
-            self.animationAmount += 1
-        })
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(
-            Animation.easeInOut(duration: 1)
-                .repeatForever(autoreverses: true)
-        )
-        
-    }
+        HStack(spacing: 0) {
+                    ForEach(0..<letters.count) { num in
+                        Text(String(self.letters[num]))
+                            .padding(5)
+                            .font(.title)
+                            .background(self.enabled ? Color.blue : Color.red)
+                            .offset(self.dragAmount)
+                            .animation(Animation.default.delay(Double(num) / 20))
+                    }
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged { self.dragAmount = $0.translation }
+                        .onEnded { _ in
+                            self.dragAmount = .zero
+                            self.enabled.toggle()
+                        }
+                )
+            }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
